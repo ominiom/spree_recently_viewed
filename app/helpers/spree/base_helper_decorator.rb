5e60cad963b1befcda1f51020b4bb79abf458fd5
Iain Wilson <iain.iw.wilson@gmail.com>
2013-08-29 16:55:44 +0100

    Compatibility

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -4,6 +4,6 @@ Spree::BaseHelper.module_eval do
   end
 
   def get_recently_viewed_products
-    Spree::Product.find_by_array_of_ids(get_recently_viewed_products_ids)
+    Spree::Product.where(:id => get_recently_viewed_products_ids)
   end
 end
